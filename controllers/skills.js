689ac049931b7528fd1fbcb2df143ea6dd734d7f
Daniel Merritt <daniel.mafou@gmail.com>
2023-04-13 09:39:36 -0400

    styling

--- controllers/skills.js
+++ controllers/skills.js
@@ -1,11 +1,51 @@
 
+import { Skill } from '../models/skill.js'
 
 function index(req, res) {
-  res.render('skills/index', {
-    skills: skills
+  Skill.find({})
+  .then(skills => {
+    res.render('skills/index', {
+      skills: skills,
+    })
+  })
+  .catch(error => {
+    console.log(error)
+    res.redirect('/')
   })
 }
 
+function newSkill(req, res) {
+  res.render('skills/new')
+}
+
+function create(req, res) {
+  console.log(req.body)
+  Skill.create(req.body)
+  .then(skill => {
+    res.redirect('/skills')
+  })
+  .catch(error => {
+    console.log(error)
+    res.redirect('/skills')
+  })
+}
+function show(req, res) {
+  Skill.findById(req.params.skillId)
+  .then (skill => {
+    res.render('skills/show', {
+      skill: skill
+    })
+  })
+  .catch(error => {
+    console.log(error)
+    res.redirect('/todos')
+  })
+}
+
+
 export {
   index,
+  newSkill as new,
+  create,
+  show,
 }
--- models/skill.js
+++ models/skill.js
@@ -2,8 +2,13 @@ import mongoose from 'mongoose'
 
 const Schema = mongoose.Schema
 
-	
-const todoSchema = new Schema({
+const skillSchema = new Schema({
   skill: String,
   difficulty: String,
 })
+
+const Skill = mongoose.model('Skill', skillSchema)
+
+export {
+  Skill,
+}
