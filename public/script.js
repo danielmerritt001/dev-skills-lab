7267bd65a45ab66dc90e66716334cca8883a4c41
Daniel Merritt <daniel.mafou@gmail.com>
2023-04-13 16:31:24 -0400

    added sound effect

---
+++ public/script.js
@@ -0,0 +1,25 @@
+
+let newSkillAudio = new Audio('/assets/audio/newskill.mp3')
+let newSkillBtn = document.getElementById('new-skill')
+let newSkillForm = document.getElementById('new-form')
+let submitTimer
+
+newSkillBtn.addEventListener('click', newSkillSound)
+newSkillForm.addEventListener('submit', delaySubmit)
+function logger() {
+  console.log(newSkillBtn)
+}
+logger()
+function delaySubmit(event){
+  console.log('submitTimer set')
+  event.preventDefault()
+  submitTimer = setTimeout(() => {
+    this.submit()
+    console.log('submitted after 5 seconds')
+  }, 5300)
+}
+
+function newSkillSound() {
+  newSkillAudio.volume = .5
+  newSkillAudio.play()
+}
